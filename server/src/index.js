f8cb227999c3ab7e1c70fc34a2aeba5443bd1e2a
Antonio Ferreira <antonio@makebetter.co.za>
2023-02-12 15:13:19 +0200

    added eslint to client and server

--- server/src/index.js
+++ server/src/index.js
@@ -1,30 +1,28 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const authRoutes = require("./routes/auth");
-const weightRoutes = require("./routes/weight");
-const swaggerUi = require("swagger-ui-express");
-const swaggerDocument = require("../swagger.json");
-const { response, request } = require("express");
+const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/auth');
+const weightRoutes = require('./routes/weight');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../swagger.json');
 const app = express();
 
 mongoose.connect(
-  "mongodb+srv://toni:1234@cluster0.elminod.mongodb.net/?retryWrites=true&w=majority",
+  'mongodb+srv://toni:1234@cluster0.elminod.mongodb.net/?retryWrites=true&w=majority',
   { useNewUrlParser: true, useUnifiedTopology: true },
   (error) => {
     if (error) {
       console.error(error);
-    } else {
     }
-  }
+  },
 );
 
 app.use(express.json());
-app.get("/", (req, res) => {
-  res.send("hello world");
+app.get('/', (req, res) => {
+  res.send('hello world');
 });
-app.use("/auth", authRoutes);
-app.use("/weight", weightRoutes);
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/auth', authRoutes);
+app.use('/weight', weightRoutes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
